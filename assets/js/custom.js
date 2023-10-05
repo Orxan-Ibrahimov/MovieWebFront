5ab6e16d085cceacef8446f596f5f4bd7cbf450d
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2023-10-05 18:06:54 +0400

    SpoilerBoxClickedEventAdded

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,32 +1,39 @@
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-  })
-
- $('.read-btn').click(function (e) { 
-    e.preventDefault();
-    $('footer .about-us').removeClass('makeBlur');
-    $('.read-btn').remove();
- });  
-
- $('.spoiler-box').click(function (e) { 
-    e.preventDefault();
-    $(this).remove();
-    $(this).parent(".spoiler").removeClass("spoiler");
-    var x = $(this).parent;
-    console.log($(this).parents());
- });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
   });
+
+  $(".read-btn").click(function (e) {
+    e.preventDefault();
+    $("footer .about-us").removeClass("makeBlur");
+    $(".read-btn").remove();
+  });
+
+  $(".spoiler-box").click(function (e) {
+    e.preventDefault();
+    $(this)
+      .parent()
+      .find(".comment-body")
+      .each(function (indexInArray, valueOfElement) {
+        $(valueOfElement).children(".spoiler").removeClass("spoiler");
+        console.log($(valueOfElement).children(".spoiler"));
+      });
+
+      $(this).remove();
+  });
+
+
+});
